e827577bbe5034c9373d73445158eb832f6fca7d
Fallen1eaves <tcyangguang@126.com>
2022-10-28 20:42:11 +0800

    string.md

---
+++ 字符串、列表、元组、字典和集合/string.py
@@ -0,0 +1,24 @@
+#字符串
+str1 ='Hello World!'
+str2 ="hello 玄魂！"
+print('声明字符串.....')
+print(str1)
+print(str2)
+
+print("str1[0]: ", str1[0])
+print("str2[1:5]: ", str2[1:5])
+##修改大小写
+name ='michelle yang'
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+##合并字符串
+first_name ='Michelle'
+last_name ='Yang'
+full_name =first_name +' '+last_name
+print(full_name)
+print("Hello, " +full_name+"!")
+##添加制表符：\t
+print('Python')
+print('\tPython')
